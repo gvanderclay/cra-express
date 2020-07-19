36cfe606297466fe13c6337a7f2d21197774f340
Gage Vander Clay <gvanderclay@gmail.com>
2020-07-19 19:23:56 -0400

    Add webpack and config

---
+++ webpack.config.server.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+const entry = { server: "./src/server/index.ts" };
+
+module.exports = {
+  mode: process.env.NODE_ENV ? process.env.NODE_ENV : "development",
+  target: "node",
+  devtool: "inline-source-map",
+  entry: entry,
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              configFile: "src/server/tsconfig.json",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
