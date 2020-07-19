cfc5b812a725f8a30d7103c9b3f076dbd9694d9e
Gage Vander Clay <gvanderclay@gmail.com>
2020-07-19 15:37:17 -0400

    Move client code to client directory

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./client/index.css";
+import App from "./client/App";
+import * as serviceWorker from "./client/serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
