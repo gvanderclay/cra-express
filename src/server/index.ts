9c22c07c0e0538bd0da4f121ace29e18a0425340
Gage Vander Clay <gvanderclay@gmail.com>
2020-07-19 19:08:52 -0400

    Add server code

---
+++ src/server/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+
+const buildDir = path.join(process.cwd() + "/build");
+const app = express();
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static(buildDir));
+
+app.get("/ping", function (req, res) {
+  return "pong";
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(buildDir, "index.html"));
+});
+
+const port = 3001;
+console.log("checking port", port);
+app.listen(port, () => {
+  console.log(`Server now listening on port: ${port}`);
+});
